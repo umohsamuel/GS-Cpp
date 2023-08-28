0899c434cd78674755eefccfbe149f35c814c6bf
Umoh Samuel Godwin <youngking074@gmail.com>
2023-08-28 19:28:29 +0100

    boilerplate

---
+++ Proj2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num{100};
+    cout << "Hello from Proj2" << endl;
+    cout << "Enter your favourite num :";
+    cin >> num;
+    cout << "Your favourite number is " << num << endl;
+    return 0;
+}
